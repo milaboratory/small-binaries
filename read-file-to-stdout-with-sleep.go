d12208f4d9f231acbf9842ad3dd26deb4ba72d17
Gleb Zakharov <snyssfx@posteo.net>
2024-07-05 12:49:12 +0200

    read-file: add additionalLogFiles

--- read-file-to-stdout-with-sleep.go
+++ read-file-to-stdout-with-sleep.go
@@ -21,7 +21,7 @@ func main() {
 	defer cancelCtx()
 
 	if len(os.Args) < 4 {
-		fatal("Usage: %s <filePath> <percentagePattern> <sleepMs> <percentageSleepMs>", os.Args[0])
+		fatal("Usage: %s <filePath> <percentagePattern> <sleepMs> <percentageSleepMs> <additionalLogFiles>", os.Args[0])
 	}
 
 	filePath := os.Args[1]
@@ -33,6 +33,10 @@ func main() {
 	percentageSleepMs, err := strconv.Atoi(os.Args[4])
 	if err != nil {
 		fatal("cannot parse percentage sleep ms: %q", os.Args[4])
+	}
+	additionalLogFiles, err := strconv.Atoi(os.Args[5])
+	if err != nil {
+		fatal("cannot parse number of additional logs files: %q", os.Args[5])
 	}
 
 	f, err := os.Open(filePath)
@@ -56,6 +60,9 @@ func main() {
 		fatal("size of a file is 0: %q", filePath)
 	}
 
+	nWroteFiles := 0
+	writeFileEveryPercent := 100.0 / float64(additionalLogFiles)
+
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	defer wg.Wait()
@@ -64,11 +71,20 @@ func main() {
 		for {
 			select {
 			case <-ctx.Done():
-				fmt.Printf("percentage goroutine is done\n")
+				fmt.Printf("percentage goroutine is done, wrote other files\n")
+				for i := range additionalLogFiles - nWroteFiles {
+					writeFile(i, "100.00")
+				}
 				return
 			case <-time.After(toDuration(percentageSleepMs)):
-				percent := strconv.FormatFloat(float64(bytesRead)/float64(bytesTotal)*100, 'f', 2, 64)
-				fmt.Printf("%s%s%% bytes read\n", percentagePattern, percent)
+				percent := float64(bytesRead) / float64(bytesTotal) * 100
+				percentStr := strconv.FormatFloat(percent, 'f', 2, 64)
+				fmt.Printf("%s%s%% bytes read\n", percentagePattern, percentStr)
+
+				if percent > writeFileEveryPercent*float64(nWroteFiles) && nWroteFiles < additionalLogFiles {
+					writeFile(nWroteFiles, percentStr)
+					nWroteFiles++
+				}
 			}
 		}
 	}()
@@ -85,6 +101,23 @@ func main() {
 			fmt.Println(scanner.Text())
 			bytesRead += int64(len(scanner.Bytes()))
 		}
+	}
+}
+
+func writeFile(idx int, percentStr string) {
+	fileName := "file" + strconv.Itoa(idx) + ".txt"
+	f, err := os.Create(fileName)
+	if err != nil {
+		fatal("cannot open file %q", fileName)
+	}
+
+	str := fmt.Sprintf("file %s, percents %s", fileName, percentStr)
+	if _, err := f.WriteString(str); err != nil {
+		fatal("cannot write string to file %q", fileName)
+	}
+
+	if err := f.Close(); err != nil {
+		fatal("cannot close file %q", fileName)
 	}
 }
 

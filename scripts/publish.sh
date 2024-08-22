61fe874448aea8669abfb38d45ab69ec254f0c4e
Korenevskiy Denis <denkoren@gmail.com>
2024-08-22 20:19:21 +0300

    fix: correct publish function name

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -36,7 +36,7 @@ publish_packages() {
   publish_package "${_bin_name}" "macosx" "aarch64"
 }
 
-publish_binaries "runenv-java-stub"
+publish_packages "runenv-java-stub"
 publish_packages "hello-world"
 publish_packages "guided-command"
 publish_packages "sleep"

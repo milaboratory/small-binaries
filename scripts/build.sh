ac5db4771a8c465fbd252e706ff1a5e9ddb3e4fc
Korenevskiy Denis <denkoren@gmail.com>
2024-12-16 16:04:42 +0300

    move hello-world to its own package

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,9 @@ set -o pipefail
 
 
 script_dir="$(cd "$(dirname "${0}")" && pwd)"
-cd "${script_dir}/.."
-repo_root="$(pwd)"
+repo_root="$(cd "${script_dir}/.." && pwd)"
 
-: "${BUILD_DIR:="${repo_root}/build"}"
+: "${BUILD_DIR:="$(pwd)/build"}"
 
 pkg_content_root() {
     local _pkg_name="${1}"
@@ -39,19 +38,6 @@ build_binary() {
         "./$(basename "${_go_name}")"
 }
 
-additional_file() {
-    local _pkg_name="${1}"
-    local _os_reg="${2}"
-    local _arch_reg="${3}"
-    local _src="${4}"
-    local _target_name="${5}"
-
-    local _pkg_root="$(pkg_content_root "${_pkg_name}" "${_os_reg}" "${_arch_reg}")"
-
-    cp -R "${_src}" "${_pkg_root}/${_target_name}"
-
-}
-
 build_binaries() {
     local _pkg_name="${1}"
     local _go_name="${2}"
@@ -75,52 +61,28 @@ build_binaries() {
     build_binary "${_pkg_name}" "${_go_name}" "${_bin_name}" "darwin" "arm64" "macosx" "aarch64"
 }
 
-add_file() {
-    local _pkg_name="${1}"
-    local _src="${2}"
-    local _dst="${3}"
+pl_pkg_name="${1}"
+go_build_target="${2}"
+result_binary_name="${3:-${1}}"
 
-    local _pkg_root
+build_binaries "${pl_pkg_name}" "${go_build_target}" "${result_binary_name}"
 
-    _pkg_root="$(pkg_content_root "${_pkg_name}" "windows" "x64")"
-    mkdir -p "${_pkg_root}"
-    cp -R "${_src}" "${_pkg_root}/${_dst}"
+# build_binaries "mnz-client" "mnz-client/cmd/mnz-client" "mnz-client"
+# build_binaries "table-converter" "table-converter/table-converter" "table-converter"
 
-    _pkg_root="$(pkg_content_root "${_pkg_name}" "linux" "x64")"
-    mkdir -p "${_pkg_root}"
-    cp -R "${_src}" "${_pkg_root}/${_dst}"
+# build_binaries "runenv-java-stub" "dump-args.go" "bin/java"
 
-    _pkg_root="$(pkg_content_root "${_pkg_name}" "linux" "aarch64")"
-    mkdir -p "${_pkg_root}"
-    cp -R "${_src}" "${_pkg_root}/${_dst}"
+# build_binaries "runenv-python-stub" "runenv-python-stub.go" "bin/python"
+# build_binaries "runenv-python-stub" "runenv-python-stub.go" "bin/pip"
 
-    _pkg_root="$(pkg_content_root "${_pkg_name}" "macosx" "x64")"
-    mkdir -p "${_pkg_root}"
-    cp -R "${_src}" "${_pkg_root}/${_dst}"
+# build_binaries "hello-world" "hello-world.go" "hello-world"
+# build_binaries "guided-command" "guided-command.go" "guided-command"
+# build_binaries "sleep" "sleep.go" "sleep"
+# build_binaries "read-with-sleep" "read-file-to-stdout-with-sleep.go" "read-with-sleep"
 
-    _pkg_root="$(pkg_content_root "${_pkg_name}" "macosx" "aarch64")"
-    mkdir -p "${_pkg_root}"
-    cp -R "${_src}" "${_pkg_root}/${_dst}"
-}
-
-rm -rf "${script_dir}/${BUILD_DIR}"
-
-build_binaries "mnz-client" "mnz-client/cmd/mnz-client" "mnz-client"
-build_binaries "table-converter" "table-converter/table-converter" "table-converter"
-
-build_binaries "runenv-java-stub" "dump-args.go" "bin/java"
-
-build_binaries "runenv-python-stub" "runenv-python-stub.go" "bin/python"
-build_binaries "runenv-python-stub" "runenv-python-stub.go" "bin/pip"
-
-build_binaries "hello-world" "hello-world.go" "hello-world"
-build_binaries "guided-command" "guided-command.go" "guided-command"
-build_binaries "sleep" "sleep.go" "sleep"
-build_binaries "read-with-sleep" "read-file-to-stdout-with-sleep.go" "read-with-sleep"
-
-pl-pkg build packages --all-platforms
+pl-pkg build --all-platforms
 
 echo ""
-echo "All binaries are saved to '$(pwd)/${BUILD_DIR}'"
+echo "All binaries are saved to '${BUILD_DIR}'"
 echo "All packages are saved to '$(pwd)/pkg-*.tgz' archives"
 echo ""
